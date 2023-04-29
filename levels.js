e5cf2c9285a53856df909eb31f9692711b167a70
Anmariru <anna.rumpl@gmx.net>
2023-04-29 16:30:36 +0200

    Movement is added

--- levels.js
+++ levels.js
@@ -1,5 +1,105 @@
+//level 6: go up and down the stairs
+const getStairsMovementDirection = function(stairNumber, isClimbingStairs){
+  if (isClimbingStairs === false){
+    return (stairNumber %2 === 1? "right": "down");
+  }
+  else if (isClimbingStairs === true){
+    return (stairNumber %2 === 1? "right": "up");
+  }
+};
 
+//level 6: Zig Zag fun
+const getZigZagMovementDirection = function(step){
+  if (step % 6 === 0){
+    return "up";
+  }
+  else if (step % 3 === 0){
+    return "down";
+  }
+  else {
+    return "right";
+  }
+};
 
+//level 6: Controlling my puppet
+const manuallyControl = function(key){
+  switch (key){
+    case "ArrowLeft":
+    case "KeyA":
+      moveDirection ("left");
+      break;
+    case "ArrowRight":
+    case "KeyD":
+      moveDirection ("right");
+      break;
+    case "ArrowUp":
+    case "KeyW":
+      moveDirection ("up");
+      break;
+    case "ArrowDown":
+    case "KeyS":
+      moveDirection ("down");
+      break;
+  }
+};
+
+const givePotion2Answer = function(list){
+  let sum = 0;
+  for (let i = 0; i < list.length; i++){
+    if (list[i] % 2 === 0){
+      sum += list[i];
+    }
+  }
+  return sum;
+};
+
+const givePotion3Answer = function(){
+  
+};
+
+const givePotion4Answer = function(){
+
+};
+
+const givePotion5Answer = function(){
+
+};
+
+const givePotion6Answer = function(){
+
+};
+
+const givePotion7Answer = function(){
+
+};
+
+const givePotion8Answer = function(){
+
+};
+
+const givePotion9Answer = function(){
+
+};
+
+const givePotion10Answer = function(){
+
+};
+
+const givePotion11Answer = function(){
+
+};
+
+const givePotion12Answer = function(){
+
+};
+
+const level7Move = function(){
+
+};
+
+const level8Move = function(){
+
+};
 // DON'T MODIFY THE CODE BELOW THIS LINE
 
 let toExport;
