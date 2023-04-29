9af1bbc43824ffd2c0f977425d47950380e2d957
franziskaotto <franziska.otto@gmx.at>
2023-04-29 16:28:20 +0200

    started my work

--- levels.js
+++ levels.js
@@ -1,4 +1,151 @@
 
+function getStairsMovementDirection (stairNumber, isClimbingStairs){
+ 
+
+// kann man noch kürzer schreiben
+  
+  // if (stairNumber === 1 && isClimbingStairs === false) {
+  //   return "down"  
+  // } else if (stairNumber === 2 && isClimbingStairs === false) {
+  //   return "right"
+  // } else if (stairNumber === 3 && isClimbingStairs === false) {
+  //   return "down"
+  // } else if (stairNumber === 4 && isClimbingStairs === false) {
+  //   return "right"
+  // } else if (stairNumber === 5 && isClimbingStairs === false) {
+  //   return "down"
+  // } else if (stairNumber === 6 && isClimbingStairs === false) {
+  //   return "right"
+  // } else if (stairNumber === 7 && isClimbingStairs === false) {
+  //   return "down"
+  // }
+
+  if (stairNumber % 2 === 0 && isClimbingStairs === false) {
+    return "down"
+  } else if (stairNumber % 2 !== 0 && isClimbingStairs === false) {
+    return "right"
+  } else if (stairNumber % 2 === 0 && isClimbingStairs === true) {
+    return "up"
+  } else if (stairNumber % 2 !== 0 && isClimbingStairs === true) {
+    return "right"
+  }
+}
+
+
+//stairnumber ist die anzahl ans stufen!
+//isclimbingstairs ist erst ab dem moment wo er literally beginnt die stairs zu climben
+
+
+
+
+function getZigZagMovementDirection (step) {
+  if (step === 0) {
+    return "up"
+  } else if (step === 1) {
+    return "right"
+  } else if (step === 2) {
+    return "right"
+  } else if (step === 3) {
+    return "down"
+  } else if (step === 4) {
+    return "right"
+  } else if (step === 5) {
+    return "right"
+  } else if (step === 6) {
+    return "up"
+  } else if (step === 7) {
+    return "right"
+  } else if (step === 8) {
+    return "right"
+  } else if (step === 9) {
+    return "down"
+  } 
+  
+  //kann man noch verkürzen bei up and down, floor
+}
+
+function manuallyControl (key) {
+  if ("ArrowRight" === key) {
+    return moveDirection("right")
+  } else if ("ArrowLeft" === key) {
+    return moveDirection("left")
+  } else if ("ArrowUp" === key) {
+    return moveDirection("up")
+  } else if ("ArrowDown" === key) {
+    return moveDirection("down")
+  }
+}
+
+
+//POTIONS functions
+
+function givePotion2Answer (list) {
+  let sum = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (highestNum )
+  }
+  
+}
+
+function givePotion3Answer () {
+  
+}
+
+function givePotion4Answer () {
+  
+}
+
+function givePotion5Answer () {
+  
+}
+
+
+function givePotion6Answer () {
+  
+}
+
+function givePotion7Answer () {
+  
+}
+
+
+function givePotion8Answer () {
+  
+}
+
+
+function givePotion9Answer () {
+  
+}
+
+function givePotion10Answer () {
+
+
+}
+
+function givePotion11Answer () {
+  
+}
+
+function givePotion12Answer () {
+  
+}
+
+
+
+//Level 7 Dungeon
+function level7Move () {
+
+}
+
+//level 8 Dungeon
+function level8Move () {
+
+}
+
+
+
+
 
 // DON'T MODIFY THE CODE BELOW THIS LINE
 
